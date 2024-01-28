f00746a2a1a524afc51a3f6e44be4f5e9e5540ba
harshit71 <harshit.raj@itential.com>
2024-01-28 20:33:19 +0530

    week 3 few question added

---
+++ 2-Questions/3-Week/1-Sort-Array-Wave/0-statement.js
@@ -0,0 +1,30 @@
+/*
+
+Problem statement
+You have been given an unsorted array ‘ARR’.
+
+Your task is to sort the array in such a way that the array looks like a wave array.
+
+Example:
+If the given sequence ‘ARR’ has ‘N’ elements then the sorted wave array looks like - 
+‘ARR[0] >= ARR[1]’ and ‘ARR[1] <= ARR[2]’
+‘ARR[2] >= ARR[3]’ and ‘ARR[3] <= ARR[4]’
+‘ARR[4] >= ARR[5]’ and ‘ARR[5] <= ARR[6]’  And so on.
+
+
+
+Sample Input 1:
+2
+5 
+2 3 1 4 2
+6
+4 3 2 5 4 1
+Sample Output 1:
+2 1 3 2 4
+4 3 5 2 4 1
+Explanation of sample input 1:
+Test Case 1:
+For given 'ARR' = { 2, 3, 1, 4, 2 } one possible sorted wave form array is { 2, 1, 3, 2, 4 }
+
+
+*/
