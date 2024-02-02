fd851686ba2300fd3296049a72357806736ee3f8
harshit71 <harshit.raj@itential.com>
2024-02-02 22:54:19 +0530

    container with most water solved using BF and two pointer

--- 2-Questions/3-Week/7-Container-with-most-water/1-solution.js
+++ 2-Questions/3-Week/7-Container-with-most-water/1-solution.js
@@ -1,1 +1,21 @@
-//
+/*
+
+BF Idea :-
+------------------------------------------------------------------------------------------------
+Iterate through the array and look for maxm area
+
+*/
+
+var maxArea = function (height) {
+  area = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = 0; j < height.length; j++) {
+      currArea = (j - i) * Math.min(height[i], height[j]);
+      if (currArea > area) {
+        area = currArea;
+      }
+    }
+  }
+
+  return area;
+};
