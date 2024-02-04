70b8aed3e11553d9f62db19d182b5eaf43aea038
harshit71 <harshit.raj@itential.com>
2024-02-04 13:20:18 +0530

    two pointer - find max diff in array

---
+++ 2-Questions/3-Week/11-Find-Max-Diff-In-Array/2-solution.js
@@ -0,0 +1,40 @@
+/*
+
+Efficient Idea 
+----------------------------------------------------------------
+
+1. Put Two pointer one at 0th index and second at 1st index
+
+2. Now We need to maintain two things --> minElement and maxDiff
+
+3. Start iterating through array and update minElement if arr[i] is smaller.
+
+4. And also update maxDiff with Maximum of (maxDiff,currentDiff)
+
+5. Keep doing this until second pointer reaches to end of array.
+
+*/
+
+var maximumDifference = function (arr) {
+  let min = Infinity,
+    maxDiff = -1;
+
+  let i = 0,
+    j = 1;
+
+  while (j < arr.length) {
+    if (arr[j] > arr[i]) {
+      min = Math.min(min, arr[i]);
+      diff = arr[j] - min;
+      maxDiff = Math.max(maxDiff, diff);
+      console.log(maxDiff);
+      i++;
+      j++;
+    } else {
+      i++;
+      j++;
+    }
+  }
+
+  return maxDiff;
+};
