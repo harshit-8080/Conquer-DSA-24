6a996dc686c8b7da64c19abd4a3878dac92bed71
harshit71 <harshit.raj@itential.com>
2024-02-04 19:25:48 +0530

    factorial using recursion

--- 2-Questions/4-Week/1-Recursion/1-Factorial-Recursion/1-solution.js
+++ 2-Questions/4-Week/1-Recursion/1-Factorial-Recursion/1-solution.js
@@ -1,1 +1,11 @@
-//
+function factorial(n) {
+  if (n == 1 || n == 0) {
+    return 1;
+  }
+
+  let factorialExceptN = factorial(n - 1);
+  return n * factorialExceptN;
+}
+
+let x = factorial(5);
+console.log(x);
