2df93e3d04daa666eab6ed1d2c4fe0c7459a6465
harshit71 <harshit.raj@itential.com>
2024-02-08 12:00:48 +0530

    improved variable name

--- 2-Questions/3-Week/14-Partition-Array-Around-Pivot/2-solution.js
+++ 2-Questions/3-Week/14-Partition-Array-Around-Pivot/2-solution.js
@@ -1,19 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
 var pivotArray = function (arr, pivot) {
   let output = new Array(arr.length).fill(pivot);
 
-  let left = 0,
-    right = arr.length - 1;
+  let nextSmaller = 0,
+    nextGreater = arr.length - 1;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < pivot) {
-      output[left] = arr[i];
-      left++;
+      output[nextSmaller] = arr[i];
+      nextSmaller++;
     }
   }
 
   for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] > pivot) {
-      output[right] = arr[i];
-      right--;
+      output[nextGreater] = arr[i];
+      nextGreater--;
     }
   }
 
