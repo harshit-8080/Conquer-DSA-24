03589593529930e638a4da22cc8c21b4fb08ed73
harshit71 <harshit.raj@itential.com>
2024-02-13 01:33:10 +0530

    first and last occurence in sorted array

---
+++ 0-Pattern_Tracks/4-Binary-Search/2-Search-Target-Reverse-Sorted-Array/1-solution.js
@@ -0,0 +1,18 @@
+function binarysearchReverseSorted(arr, n, target) {
+  let left = 0,
+    right = n - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] == target) {
+      return mid;
+    } else if (target < arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
---
+++ 0-Pattern_Tracks/4-Binary-Search/5-First-&-Last-Occurence-Sorted-Array/1-solution.js
@@ -0,0 +1,46 @@
+///* https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+var searchRange = function (arr, x) {
+  let n = arr.length;
+  let result = new Array(2).fill(-1);
+
+  let left = 0,
+    right = n - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] == x) {
+      if (mid == 0 || arr[mid] != arr[mid - 1]) {
+        result[0] = mid;
+        break;
+      } else {
+        right = mid - 1;
+      }
+    } else if (x > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  left = 0;
+  right = n - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] == x) {
+      if (mid == n - 1 || arr[mid] != arr[mid + 1]) {
+        result[1] = mid;
+        break;
+      } else {
+        left = mid + 1;
+      }
+    } else if (x > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return result;
+};
