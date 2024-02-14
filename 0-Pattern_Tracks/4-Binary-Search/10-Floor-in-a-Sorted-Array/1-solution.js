63a6ca71a4cb1e9e701c88a1c40c82186a08d011
harshit71 <harshit.raj@itential.com>
2024-02-14 22:44:06 +0530

    floor in sorted array

---
+++ 0-Pattern_Tracks/4-Binary-Search/10-Floor-in-a-Sorted-Array/1-solution.js
@@ -0,0 +1,28 @@
+//* https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+// * Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x.
+
+function findFloor(arr, n, x) {
+  let left = 0,
+    right = n - 1;
+  let mid;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] <= x) {
+      if (mid == n - 1) {
+        return mid;
+      } else if (x < arr[mid + 1]) {
+        return mid;
+      } else {
+        left = mid + 1;
+      }
+    } else if (arr[mid] < x) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
