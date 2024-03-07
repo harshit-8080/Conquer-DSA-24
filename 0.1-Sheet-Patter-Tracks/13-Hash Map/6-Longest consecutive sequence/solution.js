38736b9aba0321eb6522d7553a83cfd36ebbfbb8
harshit71 <harshit.raj@itential.com>
2024-03-08 00:34:21 +0530

    Longest Consecutive Sequence

---
+++ 0.1-Sheet-Patter-Tracks/13-Hash Map/6-Longest consecutive sequence/solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+  let map = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] != undefined) {
+      continue;
+    } else {
+      map[arr[i]] = 1;
+    }
+  }
+
+  let resultMax = 1;
+  let curr = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i] - 1] != undefined) {
+      continue;
+    } else {
+      let curElement = arr[i];
+      while (map[curElement + 1] != undefined) {
+        curr++;
+        curElement++;
+      }
+      resultMax = Math.max(resultMax, curr);
+      curr = 1;
+    }
+  }
+
+  return Math.max(resultMax, curr);
+};
