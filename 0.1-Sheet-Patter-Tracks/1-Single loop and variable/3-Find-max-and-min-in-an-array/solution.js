f7b1c5c1996ed8a77870d986071a68c411a557c5
harshit71 <harshit.raj@itential.com>
2024-03-18 23:50:18 +0530

    max min array pair check

--- 0.1-Sheet-Patter-Tracks/1-Single loop and variable/3-Find-max-and-min-in-an-array/solution.js
+++ 0.1-Sheet-Patter-Tracks/1-Single loop and variable/3-Find-max-and-min-in-an-array/solution.js
@@ -1,13 +1,11 @@
 // * https://www.enjoyalgorithms.com/blog/find-the-minimum-and-maximum-value-in-an-array
 // * https://www.geeksforgeeks.org/problems/max-min/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
 
-function findSum(arr, n) {
+function indSum(arr, n) {
   let max = arr[0];
   let min = arr[0];
 
-  let i = 1;
-
-  while (i < n) {
+  for (let i = 1; i < arr.length; i = i + 2) {
     if (arr[i] > arr[i + 1]) {
       if (arr[i] > max) {
         max = arr[i];
@@ -23,10 +21,7 @@ function findSum(arr, n) {
         min = arr[i];
       }
     }
-
-    i++;
   }
 
   return min + max;
-  //   return [max,min]
 }
