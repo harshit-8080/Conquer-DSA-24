97f1a33d0d47549bb65dba6aab68d9d62214b293
harshit71 <harshit.raj@itential.com>
2024-03-19 00:23:41 +0530

    v mountain updated

--- 0.1-Sheet-Patter-Tracks/1-Single loop and variable/4-Valid-Mountain-Array/solution.js
+++ 0.1-Sheet-Patter-Tracks/1-Single loop and variable/4-Valid-Mountain-Array/solution.js
@@ -1,20 +1,20 @@
-// * https://leetcode.com/problems/valid-mountain-array/
-// * https://www.enjoyalgorithms.com/blog/valid-mountain-array
-
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
 var validMountainArray = function (arr) {
   let peakCount = 0;
-  let i = 1;
-  let n = arr.length;
-  while (i < n - 1) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
       peakCount++;
-    } else if (arr[i - 1] > arr[i] && arr[i + 1] > arr[i]) {
+      if (peakCount > 1) {
+        return false;
+      }
+    } else if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1]) {
       return false;
-    } else if (arr[i - 1] == arr[i] || arr[i] == arr[i + 1]) {
+    } else if (arr[i] == arr[i - 1] || arr[i] == arr[i + 1]) {
       return false;
     }
-
-    i++;
   }
 
   return peakCount == 1 ? true : false;
