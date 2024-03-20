dd0fbc1bb1a25ca82139b5343b80cdab3a1e6286
harshit71 <harshit.raj@itential.com>
2024-03-20 20:20:46 +0530

    removing duplicates files/folder

--- 2-Questions/2-Week/1-Max-Min_From-Array/0-statments-example.js
+++
@@ -1,6 +0,0 @@
-/**
- * Given an array A of size N of integers.
- * Your task is to find the minimum and maximum element in the array.
- */
-
-// https://www.enjoyalgorithms.com/blog/find-the-minimum-and-maximum-value-in-an-array/
